2c715b13d24e8e5c1df6cd7bd15718b10340d4a8
William <byteblock6@gmail.com>
2024-12-09 14:52:07 +0800

    sbt mint test

--- sbt-minter/programs/sbt-minter/src/instructions/sbt_mint.rs
+++ sbt-minter/programs/sbt-minter/src/instructions/sbt_mint.rs
@@ -12,26 +12,33 @@ use crate::errors::*;
 #[derive(Accounts)]
 pub struct SbtMint<'info> {
     #[account(mut)]
-    pub payer: Signer<'info>,
+    pub mint_authority: Signer<'info>,
+
+    pub recipient: SystemAccount<'info>,
 
     #[account(mut)]
     pub mint_account: Account<'info, Mint>,
 
     #[account(
         init_if_needed,
-        payer = payer,
+        payer = mint_authority,
         associated_token::mint = mint_account,
-        associated_token::authority = payer,
+        associated_token::authority = recipient,
     )]
-    pub token_account: Account<'info, TokenAccount>,
+    pub associated_token_account: Account<'info, TokenAccount>,
 
-    #[account(init, payer = payer, space = 1000, seeds = [b"sbt_info", payer.key().as_ref()], bump)]
+    #[account(
+        init, 
+        payer = mint_authority, 
+        space = 1000, 
+        seeds = [b"sbt_info", mint_authority.key().as_ref()], 
+        bump
+    )]
     pub sbt_info: Account<'info, SbtInfo>,
 
     pub token_program: Program<'info, Token>,
-    pub system_program: Program<'info, System>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-    pub rent: Sysvar<'info, Rent>,
+    pub system_program: Program<'info, System>,
 }
 
 pub fn mint_sbt_token(ctx: Context<SbtMint>, name: String, photo: String, twitterID: String, discordID: String, telegramID: String) -> Result<()> {
@@ -51,11 +58,11 @@ pub fn mint_sbt_token(ctx: Context<SbtMint>, name: String, photo: String, twitte
             ctx.accounts.token_program.to_account_info(),
             MintTo {
                 mint: ctx.accounts.mint_account.to_account_info(),
-                to: ctx.accounts.token_account.to_account_info(),
-                authority: ctx.accounts.payer.to_account_info(),
+                to: ctx.accounts.associated_token_account.to_account_info(),
+                authority: ctx.accounts.mint_authority.to_account_info(),
             },
         ),
-        1,
+        5,
     )?;
 
     Ok(())
--- sbt-minter/tests/bankrun.test.ts
+++ sbt-minter/tests/bankrun.test.ts
@@ -1,41 +1,48 @@
-import { describe, it } from 'node:test';
+import { describe, it, before } from 'node:test';
 import * as anchor from '@coral-xyz/anchor';
-import { Keypair } from '@solana/web3.js';
-import { PublicKey } from '@solana/web3.js';
+import { Keypair, PublicKey } from '@solana/web3.js';
 import { BankrunProvider } from 'anchor-bankrun';
 import { startAnchor } from 'solana-bankrun';
-import type { CreateToken } from '../target/types/create_token';
+import { getAssociatedTokenAddressSync } from '@solana/spl-token';
+import type { SbtMinter } from '../target/types/sbt_minter';
+import * as helpers from "./helpers";
 
-const IDL = require('../target/idl/create_token.json');
+const IDL = require('../target/idl/sbt_minter.json');
 const PROGRAM_ID = new PublicKey(IDL.address);
 const METADATA_PROGRAM_ID = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s');
 
-describe('Bankrun example', async () => {
-  const context = await startAnchor(
-    '',
-    [
-      { name: 'create_token', programId: PROGRAM_ID },
-      { name: 'token_metadata', programId: METADATA_PROGRAM_ID },
-    ],
-    [],
-  );
-  const provider = new BankrunProvider(context);
-  const payer = provider.wallet as anchor.Wallet;
-  const program = new anchor.Program<CreateToken>(IDL, provider);
+describe('Bankrun example', () => {
+  let provider: BankrunProvider;
+  let payer: anchor.Wallet;
+  let program: anchor.Program<SbtMinter>;
+  let mintKeypair: Keypair;
+  let context: any;
+
+  before(async () => {
+    context = await startAnchor(
+      '',
+      [
+        { name: 'sbt_minter', programId: PROGRAM_ID },
+        { name: 'token_metadata', programId: METADATA_PROGRAM_ID },
+      ],
+      [],
+    );
+    provider = new BankrunProvider(context);
+    anchor.setProvider(provider);
+    payer = provider.wallet as anchor.Wallet;
+    program = new anchor.Program(IDL, provider);
+    mintKeypair = new Keypair();
+  });
 
   const metadata = {
-    name: 'Solana Gold',
-    symbol: 'GOLDSOL',
-    uri: 'https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/spl-token.json',
+    name: 'SBT',
+    symbol: 'SBTSOL', 
+    uri: 'https://raw.githubusercontent.com/miracleAI-Lab/solana-contracts-examples/refs/heads/main/metadata/sbt-token.json',
   };
 
-  it('Create an SPL Token!', async () => {
-    // Generate new keypair to use as address for mint account.
-    const mintKeypair = new Keypair();
-
-    // SPL Token default = 9 decimals
+  it('Create an SBT Token!', async () => {
     const transactionSignature = await program.methods
-      .createTokenMint(9, metadata.name, metadata.symbol, metadata.uri)
+      .createSbtTokenMint(metadata.name, metadata.symbol, metadata.uri)
       .accounts({
         payer: payer.publicKey,
         mintAccount: mintKeypair.publicKey,
@@ -48,22 +55,43 @@ describe('Bankrun example', async () => {
     console.log(`   Transaction Signature: ${transactionSignature}`);
   });
 
-  it('Create an NFT!', async () => {
-    // Generate new keypair to use as address for mint account.
-    const mintKeypair = new Keypair();
+  it('Mint SBTtoken to your wallet!', async () => {
+    const tokenAccountATA = getAssociatedTokenAddressSync(
+      mintKeypair.publicKey,
+      payer.publicKey
+    );
 
-    // NFT default = 0 decimals
+    const userInfo = {
+      name: 'Jesse',
+      photo: 'https://w7.pngwing.com/pngs/153/594/png-transparent-solana-coin-sign-icon-shiny-golden-symmetric-geometrical-design.png',
+      twitterID: 'https://twitter.com/solana',
+      discordID: 'https://discord.com/solana', 
+      telegramID: 'https://t.me/solana',
+    };
+
     const transactionSignature = await program.methods
-      .createTokenMint(0, metadata.name, metadata.symbol, metadata.uri)
+      .mintSbtToken(userInfo.name, userInfo.photo, userInfo.twitterID, userInfo.discordID, userInfo.telegramID)
       .accounts({
-        payer: payer.publicKey,
+        mintAuthority: payer.publicKey,
+        recipient: payer.publicKey,
         mintAccount: mintKeypair.publicKey,
+        associatedTokenAccount: tokenAccountATA,
       })
-      .signers([mintKeypair])
       .rpc();
 
     console.log('Success!');
-    console.log(`   Mint Address: ${mintKeypair.publicKey}`);
+    console.log(`   Associated Token Account Address: ${tokenAccountATA}`);
     console.log(`   Transaction Signature: ${transactionSignature}`);
+
+	const [userPDA] = PublicKey.findProgramAddressSync([Buffer.from('sbt_info'), payer.publicKey.toBuffer()], program.programId);
+    const userInfoResponse = await program.account.sbtInfo.fetch(userPDA);
+    console.log(`   User Info: ${JSON.stringify(userInfoResponse)}`);
+
+	let unpackedAccount = await helpers.getTokenAccountInfoBR(
+		context.banksClient,
+		tokenAccountATA
+	  );
+
+	  console.log(`   Unpacked Account: ${Number(unpackedAccount.amount)}`);
   });
 });
